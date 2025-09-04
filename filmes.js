d7ed381e7859a18ec8ffee8625342b48ca932b17
Naiumy <naiumy.reis@edu.pucrs.br>
2025-09-04 19:55:53 -0300

    APIpronta

--- filmes.js
+++ filmes.js
@@ -1,28 +1,29 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const filmes = []
+const filmes = [];
 
 router.post('/', (req, res) => {
     const {titulo, ano} = req.body
 
     const novoFilme = {
-        id: filmes.length +1,
+        id: filmes.length + 1,
         titulo,
-        ano
-    }
+        ano,
+        reviews: []
+    };
 
     filmes.push(novoFilme);
 
     res.status(201).json({
         message: "Filme criado com sucesso!",
         filme: novoFilme
-    })
+    });
 });
 
 router.get('/', (req, res) => {
     res.json(filmes)
-})
+});
 
 router.get('/:id', (req, res) => {
   const id = parseInt(req.params.id)
@@ -30,10 +31,10 @@ router.get('/:id', (req, res) => {
 
   if (!filme) {
     return res.status(404).json({ message: "Filme não encontrado" })
-  }
+  };
 
   res.json(filme)
-})
+});
 
 router.put('/:id', (req, res) => {
     const id = parseInt(req.params.id)
@@ -42,8 +43,8 @@ router.put('/:id', (req, res) => {
     if (!filme) {
         return res.status(404).json({
             message: "Filme não encontrado"
-        })
-    }
+        });
+    };
 
     const {titulo, ano} = req.body
     if (titulo) filme.titulo = titulo
@@ -52,16 +53,108 @@ router.put('/:id', (req, res) => {
     res.json({
         message: "Filme atualizado com sucesso!",
         filme
-    })
-})
+    });
+});
 
 router.delete('/:id', (req, res) => {
     const id = parseInt(req.params.id)
 
     const index = filmes.findIndex(f => f.id === id)
-    if (index ) {
-        
-    }
-})
+    if (index === -1) {
+        return res.status(404).json({
+            message: "Filme não encontrado"
+        });
+    };
+
+    const filmeRemovido = filmes.splice(index, 1)
+    res.json({
+        message: "Filme deletado com sucesso!",
+        filme: filmeRemovido[0]
+    });
+});
+
+// CRUD de review
+router.post('/:id/reviews', (req, res) => {
+    const id = parseInt(req.params.id)
+    const filme = filmes.find(f => f.id === id)
+
+    if (!filme) {
+        return res.status(404).json({
+            message: "Filme não encontrado"
+        });
+    };
+
+    const novaReview = {
+        id: filme.reviews.length + 1,
+        comentario: req.body.comentario,
+        nota: req.body.nota
+    };
+
+    filme.reviews.push(novaReview)
+    res.status(201).json(novaReview)
+});
+
+router.get('/:id/reviews', (req, res) => {
+    const id = parseInt(req.params.id)
+    const filme = filmes.find(f => f.id === id)
+
+    if (!filme) {
+        return res.status(404).json({
+            message: "Filme não encontrado"
+        });
+    };
+
+    res.json(filme.reviews)
+});
+
+router.put('/:id/reviews/:reviewsId', (req, res) => {
+    const id =  parseInt(req.params.id)
+    const reviewsId = parseInt(req.params.reviewsId)
+    const filme = filmes.find(f => f.id === id)
+
+    if (!filme) {
+        return res.status(404).json({
+            message: "Filme não encontrado"
+        });
+    };
+
+    const review = filme.reviews.find(r => r.id === reviewsId)
+    if (!review) {
+        return res.status(404).json({
+              message: "Review não encontrada"
+        });
+    };
+
+    review.comentario = req.body.comentario || review.comentario
+    review.nota = req.body.nota || review.nota
+    
+    res.json(review)
+});
+
+router.delete('/:id/reviews/:reviewsId', (req, res) => {
+    const id =  parseInt(req.params.id)
+    const reviewsId = parseInt(req.params.reviewsId)
+    const filme = filmes.find(f => f.id === id)
+
+    if (!filme) {
+        return res.status(404).json({
+            message: "Filme não encontrado"
+        });
+    };
+
+    const index = filme.reviews.findIndex(r => r.id === reviewsId)
+    if (index === -1) {
+        return res.status(404).json({
+            message: "Review não encontrada"
+        });
+    };
+    
+    const reviewRemovida = filme.reviews.splice(index, 1)
+
+    res.json({
+        message: "Review deletada com sucesso!",
+        review: reviewRemovida[0]
+    });
+});
 
 module.exports = router
